4fe84d500c153c997d0ea5fed3b8628ad733fef6
ASUS <ASUS@DESKTOP-QOB7I2F>
2024-03-28 20:02:01 +0300

    Добавьте файлы проекта.

---
+++ Lesson3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿//1
+int P = 10;
+int A = P - 4;
+int B = A + 2;
+int sum = P + A + B;
+Console.WriteLine($"Петя взял {P} книг, Алеша взял {A} книг, Ваня взял {B} книг");
+Console.WriteLine($"Всего мальчики взяли {sum} книг");
+//2
+int firstBasket = 23;
+int secondBasket = firstBasket - 11;
+int summa = firstBasket + secondBasket;
+Console.WriteLine($"Всего яблок: {summa}");
